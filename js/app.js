e6965f8ef5d1c3fed0635708edfdccdcebb20ad3
David Mendiola <jdmmendiola@gmail.com>
2017-08-29 23:30:50 -0400

    progress

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 
 const express = require('express');
 const config = require('../config.js');
+const path = require('path');
 const Twit = require('twit');
 const app = express();
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static(path.join(__dirname, '..', 'public')));
 app.set('view engine', 'pug');
+app.set('views', '../views/');
 
 let T = new Twit(config);
 
 app.get('/', (req, res) => {
-    
+    res.render('index');
 });
 
 app.listen(3000, () => {
