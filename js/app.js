d596f9c37c3653736f50e7b008316e4dc85d0c3a
David Mendiola <jdmmendiola@gmail.com>
2017-08-29 23:54:46 -0400

    added username

--- js/app.js
+++ js/app.js
@@ -5,14 +5,20 @@ const config = require('../config.js');
 const path = require('path');
 const Twit = require('twit');
 const app = express();
+let Data = {};
 
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 let T = new Twit(config);
 
+T.get('account/verify_credentials', (err, data, response) => {
+    Data.screenName = data.screen_name;
+    console.log(Data);
+});
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {Data});
 });
 
 app.listen(3000, () => {
