670ba4cca3c9951b122c410a2e4d94023bf780a0
David Mendiola <jdmmendiola@gmail.com>
2017-08-29 22:15:38 -0400

    setup config.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-const config = require('../config.js');
+'use strict';
 
-console.log(config);
+const express = require('express');
+const config = require('../config.js');
+const Twit = require('twit');
+const app = express();
+
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+
+let T = new Twit(config);
+
+app.get('/', (req, res) => {
+    
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!');
+});
