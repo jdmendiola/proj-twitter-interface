64339213a71025b252954bfb95f3284ca7b8602e
David Mendiola <jdmmendiola@gmail.com>
2017-08-31 00:41:52 -0400

    switched api calls to promises

--- js/app.js
+++ js/app.js
@@ -11,12 +11,25 @@ app.set('view engine', 'pug');
 
 let T = new Twit(config);
 
-T.get('account/verify_credentials', (err, data, response) => {
-    Data.screenName = data.screen_name;
-    Data.profileImage = originalImageSize(data.profile_image_url_https);
-    Data.profileBgImage = data.profile_background_image_url_https;
-    console.log(Data);
-});
+T.get('account/verify_credentials')
+    .catch(function (err){
+        console.log('caught error', err.stack);
+    })
+    .then(function (result){
+        
+        Data.screenName = result.data.screen_name;
+        Data.profileImage = originalImageSize(result.data.profile_image_url_https, '_normal');
+
+        T.get('users/profile_banner', {screen_name: Data.screenName})
+        .catch(function (err){
+            console.log('caught error', err.stack);
+        })
+        .then(function (result){
+            Data.headerImage = result.data.sizes.web_retina.url
+            console.log(Data);
+        });
+        
+    });
 
 app.get('/', (req, res) => {
     res.render('index', {Data});
@@ -26,7 +39,7 @@ app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
 });
 
-function originalImageSize(profileImage){
-    profileImage = profileImage.replace('_normal', '');
+function originalImageSize(profileImage, strippedText){
+    profileImage = profileImage.replace(strippedText, '');
     return profileImage;
 }
