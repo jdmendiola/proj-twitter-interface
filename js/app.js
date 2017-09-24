a685735f608f2f249e989a8a1fa4b5c51c9c8673
David Mendiola <jdmmendiola@gmail.com>
2017-09-23 23:39:14 -0400

    beautify

--- js/app.js
+++ js/app.js
@@ -15,36 +15,38 @@ let T = new Twit(config);
 app.get('/', (req, res) => {
 
     Promise.all([
-        getAccountInfo()
-    ])
-    .then(values => {
-
-        Data.accountInfo = values[0];
-
-        Promise.all([
-            getAccountBanner(Data.accountInfo.screenName),
-            getRecentTweets(Data.accountInfo.screenName, 5),
-            getRecentFriends(Data.accountInfo.screenName, 5),
-            getRecentSentMessage(5)
+            getAccountInfo()
         ])
         .then(values => {
 
-            Data.headerImage = values[0];
-            Data.tweet = values[1];
-            Data.friendsList = values[2]
-            Data.sentMessages = values[3]
-            res.render('index', {Data});
+            Data.accountInfo = values[0];
+
+            Promise.all([
+                    getAccountBanner(Data.accountInfo.screenName),
+                    getRecentTweets(Data.accountInfo.screenName, 5),
+                    getRecentFriends(Data.accountInfo.screenName, 5),
+                    getRecentSentMessage(5)
+                ])
+                .then(values => {
+
+                    Data.headerImage = values[0];
+                    Data.tweet = values[1];
+                    Data.friendsList = values[2]
+                    Data.sentMessages = values[3]
+                    res.render('index', {
+                        Data
+                    });
+
+                });
 
         });
-
-    });
 })
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
 });
 
-function getAccountInfo(){
+function getAccountInfo() {
     return T.get('account/verify_credentials')
         .catch(error => {
             console.log(error);
@@ -59,8 +61,10 @@ function getAccountInfo(){
         })
 }
 
-function getAccountBanner(twitterId){
-    return T.get('users/profile_banner', {screen_name: twitterId})
+function getAccountBanner(twitterId) {
+    return T.get('users/profile_banner', {
+            screen_name: twitterId
+        })
         .catch(error => {
             console.log(error);
         })
@@ -69,23 +73,26 @@ function getAccountBanner(twitterId){
         });
 }
 
-function getRecentTweets(twitterId, count){
-    return T.get('statuses/user_timeline', {user_id: twitterId, count: count})
+function getRecentTweets(twitterId, count) {
+    return T.get('statuses/user_timeline', {
+            user_id: twitterId,
+            count: count
+        })
         .catch(error => {
             console.log(error);
         })
         .then(values => {
 
             let tweets = values.data.map(value => {
-                
+
                 let tweetProps = {};
-                
+
                 tweetProps.tweet = value.text;
                 tweetProps.likes = value.favorite_count;
                 tweetProps.retweets = value.retweet_count;
                 tweetProps.time = moment(value.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('h:mma, DD/MM/YY');
-                
-                if (value.retweeted_status != null || undefined){
+
+                if (value.retweeted_status != null || undefined) {
                     tweetProps.retweetLikes = value.retweeted_status.favorite_count;
                 }
 
@@ -98,15 +105,18 @@ function getRecentTweets(twitterId, count){
         });
 }
 
-function getRecentFriends(twitterId, count){
-    return T.get('friends/list', {screen_name: twitterId, count: count})
+function getRecentFriends(twitterId, count) {
+    return T.get('friends/list', {
+            screen_name: twitterId,
+            count: count
+        })
         .catch(error => {
             console.log(error);
         })
         .then(values => {
 
             let friends = values.data.users.map(value => {
-                
+
                 let friend = {};
 
                 friend.name = value.name;
@@ -122,28 +132,30 @@ function getRecentFriends(twitterId, count){
         });
 }
 
-function getRecentSentMessage(count){
-    return T.get('direct_messages/sent', {count: count})
-    .catch(error => {
-        console.log(error.stack);
-    })
-    .then(values => {
-        
-        let message = values.data.map(value => {
+function getRecentSentMessage(count) {
+    return T.get('direct_messages/sent', {
+            count: count
+        })
+        .catch(error => {
+            console.log(error.stack);
+        })
+        .then(values => {
 
-            let sentMessage = {};
+            let message = values.data.map(value => {
 
-            sentMessage.body = value.text;
-            sentMessage.time = moment(value.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('h:mma');
-            sentMessage.date = moment(value.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('DD/MM/YY');
-            sentMessage.receipient = value.recipient_screen_name;
+                let sentMessage = {};
 
-            return sentMessage;
+                sentMessage.body = value.text;
+                sentMessage.time = moment(value.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('h:mma');
+                sentMessage.date = moment(value.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('DD/MM/YY');
+                sentMessage.receipient = value.recipient_screen_name;
+
+                return sentMessage;
+
+            });
+
+            return message
 
         });
-
-        return message
-
-    });
 
 }
